6cf16ae36d41d01511a56cd96856ae8915515673
Vlad <anikanovvlad@yandex.ru>
2022-02-08 22:54:43 +0500

    done requests

--- push.py
+++ push.py
@@ -5,4 +5,4 @@ import requests
 password = "123"
 password_hash_object = hashlib.sha256(password.encode("utf-8"))
 password_hex_dig = password_hash_object.hexdigest()
-print(requests.get(f"https://watersensors.herokuapp.com/check_user", params={"l": 123, "p": password_hex_dig}))
+print(requests.get(f"https://watersensors.herokuapp.com/check_user?l=123&p={password_hex_dig}").text)
