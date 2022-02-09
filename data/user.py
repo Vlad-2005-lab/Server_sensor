9161d112add53ea50fd2a2af2d36975d01fe77da
Vlad <anikanovvlad@yandex.ru>
2022-02-09 15:09:37 +0500

    create user test

--- data/user.py
+++ data/user.py
@@ -7,7 +7,7 @@ class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'user'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     login = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=True)
-    password = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=True)
-    mail = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=True)
+    password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    mail = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     sensors = sqlalchemy.Column(sqlalchemy.String, default="")
     extra = sqlalchemy.Column(sqlalchemy.String, default="")
