87e0b622115ce24f845175f4796476d65b718370
Vlad <anikanovvlad@yandex.ru>
2022-02-12 22:44:53 +0500

    /set_data_sensor have done

--- push.py
+++ push.py
@@ -1,8 +1,11 @@
 import requests
-import hashlib
+import time
 
 
-# requests.request("POST", f"https://watersensors.herokuapp.com/add_new_sensor?p={password}")
+# for _ in range(10):
+#     requests.request("POST", f"https://watersensors.herokuapp.com/add_new_sensor?p={password}")
+#     time.sleep(1)
+# requests.request("POST", f"https://watersensors.herokuapp.com/add_new_sensor?p=")
 # password_hash_object = hashlib.sha256(password.encode("utf-8"))
 # password_hex_dig = password_hash_object.hexdigest()
 # print(password_hex_dig)
